5b2cacc2e14353f4a91843e4140c07337e4dc98b
newbilius <newbilius@gmail.com>
2018-11-21 23:32:00 +0500

    Попробовал ещё немного обобщить генераторы, а потом понял, что сильнее не очень то и обобщаются. Но чуток читаемей вроде стало

---
+++ src/main/java/com/newbilius/HabrStatisticCollector/AnalyticsGenerators/BaseAnalyticsGenerator.java
@@ -0,0 +1,28 @@
+package com.newbilius.HabrStatisticCollector.AnalyticsGenerators;
+
+import com.newbilius.HabrStatisticCollector.CSVWriteHelper;
+import com.newbilius.HabrStatisticCollector.HabrItem;
+
+import java.io.IOException;
+
+public abstract class BaseAnalyticsGenerator implements IAnalyticsGenerator {
+    public abstract String fileName();
+
+    public abstract String[] getHeaders();
+
+    public abstract String[] getData(HabrItem item);
+
+    @Override
+    public void generate(HabrItem[] items) {
+        try {
+            try (var csvWriter = CSVWriteHelper.createCSVWriter(fileName() + ".csv")) {
+                csvWriter.writeNext(getHeaders());
+
+                for (var item : items)
+                    csvWriter.writeNext(getData(item));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/newbilius/HabrStatisticCollector/AnalyticsGenerators/FullTable.java
+++ src/main/java/com/newbilius/HabrStatisticCollector/AnalyticsGenerators/FullTable.java
@@ -1,39 +1,36 @@
 package com.newbilius.HabrStatisticCollector.AnalyticsGenerators;
 
-import com.newbilius.HabrStatisticCollector.CSVWriteHelper;
 import com.newbilius.HabrStatisticCollector.HabrItem;
 
-import java.io.IOException;
-
-public class FullTable implements IAnalyticsGenerator {
+public class FullTable extends BaseAnalyticsGenerator {
     @Override
-    public void generate(HabrItem[] items) {
-        try {
-            try (var csvWriter = CSVWriteHelper.createCSVWriter("Статьи с основными параметрами.csv")) {
-                csvWriter.writeNext(new String[]{
-                        "Заголовок",
-                        "Плюсы",
-                        "Закладки",
-                        "Комментарии",
-                        "Просмотры",
-                        "URL",
-                        "Автор",
-                });
+    public String fileName() {
+        return "Статьи с основными параметрами";
+    }
 
-                for (var item : items) {
-                    csvWriter.writeNext(new String[]{
-                            item.Title,
-                            String.valueOf(item.Score),
-                            String.valueOf(item.Bookmarks),
-                            String.valueOf(item.Comments),
-                            String.valueOf(item.Views),
-                            item.Url,
-                            item.Author
-                    });
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @Override
+    public String[] getHeaders() {
+        return new String[]{
+                "Заголовок",
+                "Плюсы",
+                "Закладки",
+                "Комментарии",
+                "Просмотры",
+                "URL",
+                "Автор"
+        };
+    }
+
+    @Override
+    public String[] getData(HabrItem item) {
+        return new String[]{
+                item.Title,
+                String.valueOf(item.Score),
+                String.valueOf(item.Bookmarks),
+                String.valueOf(item.Comments),
+                String.valueOf(item.Views),
+                item.Url,
+                item.Author
+        };
     }
 }
